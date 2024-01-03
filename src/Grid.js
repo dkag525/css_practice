445baa61ce014e21b3c1b1e8cee882fff9549740
dkag525 <dkag525@gmail.com>
2024-01-03 23:13:47 +0530

    added

---
+++ src/Grid.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Grid() {
+  return (
+    <div className="Gridcontainer">
+      <div className="fitem item1">1</div>
+      <div className="fitem item2">2</div>
+      <div className="fitem item3">3</div>
+      <div className="fitem item4">4</div>
+      <div className="fitem item5">5</div>
+      {/* <div className="fitem item6">6</div>
+      <div className="fitem item7">7</div>
+      <div className="fitem item8">8</div>
+      <div className="fitem item9">9</div>
+      <div className="fitem item10">10</div> */}
+    </div>
+  );
+}
+
+export default Grid;
