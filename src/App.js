f2b79557460c4c9f235d2071e81a49e9e22dfa08
dkag525 <dkag525@gmail.com>
2024-01-03 22:09:36 +0530

    Css added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="fcontainer">
+      <div className="fitem item1">1</div>
+      <div className="fitem item2">2</div>
+      <div className="fitem item3">3</div>
+      <div className="fitem item4">4</div>
     </div>
   );
 }
